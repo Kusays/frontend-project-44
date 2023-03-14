700e9f7f3149bcd6c3c346f2d7f089ae00fd4bcf
Valeriia <vaznaapisa8@gmail.com>
2023-03-14 22:22:39 +0100

    fix logic

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -5,6 +5,15 @@ import {
   sayHi, wrongAnswer, getRandomInt, congratulations,
 } from '../src/index.js';
 
+function isPrime(number) {
+  if (number <= 1) { return false; }
+
+  for (let i = 2; i * i <= number; i += 1) {
+    if (number % i === 0) { return false; }
+  }
+  return true;
+}
+
 const name = sayHi();
 let counter = 0;
 let correctAnswer;
@@ -13,13 +22,10 @@ console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
 for (let i = 0; i < 3; i += 1) {
   const num = getRandomInt(100);
-  if (num > 1) {
-    for (let j = 2; j < num; j += 1) {
-      if (num % i === 0) {
-        correctAnswer = 'no';
-      }
-    }
+  if (isPrime(num)) {
     correctAnswer = 'yes';
+  } else {
+    correctAnswer = 'no';
   }
   console.log('Question:', num);
   const answer = readlineSync.question('Your answer: ');
